dd398ce08f5d573dac195e6154952deead1c1adc
valentine <valik.kuts@gmail.com>
2019-10-19 11:58:47 +0200

    change LoginFilter, LoginController

--- app/src/main/java/pl/edu/pjwstk/jaz/login/LoginFilter.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/login/LoginFilter.java
@@ -6,16 +6,37 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter("*")
 public class LoginFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        if (req.getRequestURI().contains("/test")) {
-            res.sendRedirect("http://google.com");
+
+//        if (req.getSession().getAttribute("username") == null) {
+//            //res.sendRedirect("login.xhtml");
+//        } else {
+//            chain.doFilter(req, res);
+//        }
+
+//        HttpSession session = req.getSession();
+//        if (session != null) {
+//            String username = (String) session.getAttribute("username");
+//            if(username == null){
+//                res.sendRedirect("login.xhtml");
+//                //res.sendRedirect("/app/index.xhtml");
+//            } else{
+//                chain.doFilter(req, res);
+//            }
+//        }
+
+        if (req.getRequestURI().contains("/index.xhtml") && req.getSession().getAttribute("username") == null) {
+            res.sendRedirect("login.xhtml");
         } else {
             chain.doFilter(req, res);
         }
+
+
     }
 }
--- app/src/main/java/pl/edu/pjwstk/jaz/webapp/LoginController.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/webapp/LoginController.java
@@ -5,18 +5,58 @@ import pl.edu.pjwstk.jaz.login.LoginRequest;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Named
 @RequestScoped
-public class LoginController {
+public class LoginController extends HttpServlet {
     @Inject
     private LoginRequest loginRequest;
 
-    public void login() {
-        System.out.println("Tried to log in using " + loginRequest.toString());
+    public String info() {
+        return loginRequest.msg();
     }
 
-    public String info() {
-        return  loginRequest.msg();
+    public void login() {
+//        if (loginRequest.getEmail() == "qqqq") {
+//            HttpSession session = request.getSession(true); // reuse existing
+//            // session if exist
+//            // or create one
+//            session.setAttribute("user", un);
+//            session.setMaxInactiveInterval(30); // 30 seconds
+//            response.sendRedirect("home.jsp");
+//        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req,
+                          HttpServletResponse res) throws ServletException, IOException {
+        //response.setContentType("text/html");
+        var respWriter = res.getWriter();
+        String name = req.getParameter("name");
+        String pwd = req.getParameter("password");
+        String username = req.getParameter("username");
+
+        if (name.equals("Test") && pwd.equals("Test1234")) {
+
+            HttpSession session = req.getSession(true); // reuse existing
+            // session if exist
+            // or create one
+            session.setAttribute("username", username);
+            session.setMaxInactiveInterval(30); // 30 seconds
+            res.sendRedirect("index.xhtml");
+        } else {
+            RequestDispatcher rd = req.getRequestDispatcher("login.xhtml");
+            respWriter.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(req, res);
+        } // TODO Auto-generated method stub
     }
 }
+
