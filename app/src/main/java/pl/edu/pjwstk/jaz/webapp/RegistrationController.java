06089ac7033263ab19fe1e6d0a75f031d98c0240
valentine <valik.kuts@gmail.com>
2019-10-27 10:22:34 +0100

    working2

--- app/src/main/java/pl/edu/pjwstk/jaz/webapp/RegistrationController.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/webapp/RegistrationController.java
@@ -10,12 +10,8 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.NoResultException;
 import javax.servlet.http.HttpSession;
-import javax.transaction.Transactional;
 import java.io.IOException;
 
-//import org.mindrot.jbcrypt.BCrypt;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 @Named
 @RequestScoped
 
@@ -36,28 +32,23 @@ public class RegistrationController {
         isUserExist = userExist;
     }
 
-//    public String info() {
-//        return user.toString();
-//    }
-public String info() {
-    return "Username exists try again";
-}
+    public String info() {
+        return "Username exists try again";
+    }
 
     public void registration() throws IOException {
 
-        if(user.isAttributesSet()){
+        if (user.isAttributesSet()) {
 
             ProfileEntity pe = null;
             try {
                 pe = pr.selectSingleResWithUsername(user.getUsername().trim());
-                //System.out.println("Username exists ");
-                //System.out.println(pe);
-            } catch (NoResultException nre){
+
+            } catch (NoResultException nre) {
                 System.out.println("Username does not exist");
             }
 
-            if(pe == null) {
-                //setIsUserExist(false);
+            if (pe == null) {
                 String plain_password = user.getPassword().trim();
                 //String pw_hash = BCrypt.hashpw(plain_password, BCrypt.gensalt());
                 String name = user.getName().trim();
@@ -66,15 +57,12 @@ public String info() {
                 pr.insert(name, suname, plain_password,
                         user.getEmail().trim(), user.getUsername().trim(), user.getBirthday().trim());
 
-
                 FacesContext facesContext = FacesContext.getCurrentInstance();
                 HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
                 session.setAttribute("name", name);
                 session.setAttribute("surname", suname);
-
-                //System.out.println(session.getAttribute("name"));
-
                 facesContext.getExternalContext().redirect("index.xhtml");
+
             } else {
                 setIsUserExist(true);
             }
