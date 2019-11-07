fbc8413299a33a96f2c3a3d61486748dd013c337
valentine <valik.kuts@gmail.com>
2019-11-07 11:43:37 +0100

    Date formatted after form action

--- app/src/main/java/pl/edu/pjwstk/jaz/login/LoginFilter.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/login/LoginFilter.java
@@ -6,30 +6,12 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter("*")
 public class LoginFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-//        if (req.getRequestURI().contains("index.xhtml") && req.getSession().getAttribute("name") == null) {
-//            res.sendRedirect("login.xhtml");
-//
-//            System.out.println("getRequestURI: " + req.getRequestURI());
-//            System.out.println("getContextPath: " + req.getContextPath());
-//
-//        } else if (req.getRequestURI().equals(req.getContextPath() + "/")) {
-//            res.sendRedirect("login.xhtml");
-////        } else if (req.getRequestURI().matches("log.xhtml")) {
-////            res.sendRedirect("login.xhtml");
-//        } else if (req.getRequestURI().matches(req.getContextPath() + "/" + "protected.xhtml")) {
-//            res.sendRedirect("login.xhtml");
-//        } else {
-//            chain.doFilter(req, res);
-//        }
-//    }
 
         if ((req.getRequestURI().contains("index.xhtml") || req.getRequestURI().equals(req.getContextPath() + "/"))
                 && req.getSession().getAttribute("name") == null) {
@@ -42,4 +24,4 @@ public class LoginFilter extends HttpFilter {
     }
 
 }
-//r.getRequestURI().matches(".*?/(javax\.faces\.resource|rfRes)/.*")
+////1111Qqqq
--- app/src/main/java/pl/edu/pjwstk/jaz/webapp/RegistrationController.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/webapp/RegistrationController.java
@@ -12,6 +12,12 @@ import javax.inject.Named;
 import javax.persistence.NoResultException;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 @Named
 @RequestScoped
@@ -44,9 +50,41 @@ public class RegistrationController {
             ProfileEntity pe = null;
             try {
                 pe = pr.selectSingleResWithUsername(user.getUsername().trim());
+                //----Date
+                String bithday = user.getBirthday().trim();
+                System.out.println(bithday);
+
+//                String dateInString = bithday;
+//                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//                LocalDate dateTime = LocalDate.parse(dateInString, formatter);
+//                System.out.println(dateTime);
+
+//                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+//                Date date = dateFormat.parse(bithday);
+//                System.out.println(dateFormat.format(date));
+
+
+                String input = bithday; //"Thu Jun 18 20:56:02 EDT 2009";  Mon Jul 07 02:00:00 CEST 1997
+                SimpleDateFormat parser = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+                Date date = parser.parse(input);
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                String formattedDate = formatter.format(date);
+                user.setBirthday(formattedDate);
+                System.out.println(user.getBirthday());
+                System.out.println(formattedDate);
+//-----------
+//                String pattern = "dd/MM/yyyy";
+//                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+//
+//                String date1 = simpleDateFormat.format(new Date());
+//                System.out.println(date1);
+                //--------
 
             } catch (NoResultException nre) {
                 System.out.println("Username does not exist");
+            } catch (ParseException e) {
+                System.out.println("ParseException");
+                e.printStackTrace();
             }
 
             if (pe == null) {
@@ -81,3 +119,5 @@ public class RegistrationController {
 
 
 }
+////1111Qqqq
+//Mon Jul 07 02:00:00 CEST 1997
