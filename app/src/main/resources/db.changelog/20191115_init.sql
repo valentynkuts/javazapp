5efa9b7721e76afed6cc82d4c65b36faa5c41e41
valentine <valik.kuts@gmail.com>
2019-11-17 12:43:35 +0100

    add product_param_val table

--- app/src/main/resources/db.changelog/20191115_init.sql
+++ app/src/main/resources/db.changelog/20191115_init.sql
@@ -21,33 +21,10 @@ CREATE TABLE product
     description  TEXT   NOT NULL,
     price  NUMERIC (9,2)   NOT NULL,
     category_id  BIGINT NOT NULL,
-    creator_id  BIGINT NOT NULL,  -- id user creator from profile
+    creator_id  BIGINT NOT NULL,  -- id_user_creator from profile
 
     FOREIGN KEY(creater_id) REFERENCES profile(id),
     FOREIGN KEY(category_id) REFERENCES category(id_category)
-);
-
-/*
-CREATE TABLE myproduct
-(
-    profile_id  BIGINT NOT NULL,  -- id user
-    product_id  BIGINT NOT NULL,
-    data    TIMESTAMP  NOT NULL,
-
-    CONSTRAINT   myproduct_pk PRIMARY KEY(profile_id, product_id),
-
-    FOREIGN KEY(profile_id) REFERENCES profile(id),
-
-    FOREIGN KEY(product_id) REFERENCES product(id_product),
-);
-*/
-
-CREATE TABLE parametr_name
-(
-    id_parametr   BIGSERIAL  PRIMARY KEY,
-    name_key VARCHAR   NOT NULL,    -- color
-    --value  VARCHAR   NOT NULL      -- grey
-
 );
 
 CREATE TABLE parametr_name
@@ -67,13 +44,12 @@ CREATE TABLE photo
 
 );
 
-CREATE TABLE product_parametr
+--product parametr => value
+CREATE TABLE product_parametrval
 (
     product_id  BIGINT NOT NULL,
     parametr_id BIGINT NOT NULL,
     value_param VARCHAR   NOT NULL      -- grey
-
-    --data    TIMESTAMP  NOT NULL,
 
     CONSTRAINT   product_parametr_pk PRIMARY KEY(product_id,parametr_id),
 
@@ -86,7 +62,6 @@ CREATE TABLE product_photo
 (
     product_id  BIGINT NOT NULL,
     photo_id BIGINT NOT NULL,
-    --data    TIMESTAMP  NOT NULL,
 
     CONSTRAINT   product_parametr_pk PRIMARY KEY(product_id,photo_id),
 
@@ -95,6 +70,3 @@ CREATE TABLE product_photo
     FOREIGN KEY(photo_id) REFERENCES photo(id_photo)
 );
 
-/*
-Zdjęć może być wiele tak samo jak parametrów.
- Dodatkowo parametr powinien być podzielony na osobną tabelę z definicją i wartością dla konkretnej aukcji. */
